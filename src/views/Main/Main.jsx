0d2f089d10d0b3f38b9fcbeca908c5055017037f
Aemilius Morgan <aemiliusmorgan@gmail.com>
2022-01-14 10:57:47 -0800

    added calendar - need to style it

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import '../../../node_modules/react-modern-calendar-datepicker';
+import DatePicker from 'react-modern-calendar-datepicker';
+import { useState } from 'react';
 
 export default function Main() {
+  const [startDate, setStartDate] = useState(null);
+  const [name, setName] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>
-      <h1>Main Page</h1>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="name"></label>
+      <input id="name" value={name} onChange={(name) => setName(name)}></input>
+      <DatePicker
+        selected={startDate}
+        onChange={(date) => setStartDate(date)}
+      />
+    </form>
   );
 }
