ce9dc6a970a733131cf60c711d47e7823412f18e
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2022-01-14 16:37:34 -0800

    added styling to home and login page

--- src/views/Auth/Auth.jsx
+++ src/views/Auth/Auth.jsx
@@ -4,6 +4,7 @@ import { Link } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
 // import { signInUser, signUpUser } from '../../services/users';
 import { getSignInUser } from '../../utils/utils';
+import Styles from './Auth.css'
 // cal fetch utils function to gain access to user data from back end response
 
 export default function Auth({ isSigningIn = false }) {
@@ -17,17 +18,29 @@ export default function Auth({ isSigningIn = false }) {
         const user = await getSignInUser();
         setUser(user);
         console.log('user', user);
-        location.replace('https://uptupfmuuwpcbmudkedl.supabase.co/auth/v1/authorize?provider=google')
+        // location.replace('https://uptupfmuuwpcbmudkedl.supabase.co/auth/v1/authorize?provider=google')
     } catch (error) {
       throw error;
     }
   };
 
   return (
-    <section>
-      <h2>Welcome!</h2>
-
-    <button onClick={handleSubmit}>CLICK HERE PLEASE sign in with google, it cool</button>
+    <>
+    <div>
+    <section className={StyleSheet.authSect}>
+      <h2 className={Styles.authWelcome}>Welcome!</h2>
     </section>
+<div className={Styles.btnDiv}>
+    <button className={Styles.authBtn} onClick={handleSubmit}>
+  <section className={Styles.btnSect}>
+    <div className={Styles.imgDiv}>
+    <img  className={Styles.googleBtn} src='https://freesvg.org/img/1534129544.png' />
+    </div>
+    <p className={Styles.btnP}>Sign in with Google</p>
+    </section>
+    </button>
+</div>
+</div>
+    </>
   );
 }
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,18 +1,27 @@
 import { Link, Redirect } from 'react-router-dom';
 import { useUser } from '../../context/UserContext';
-
+import Styles from './Home.css'
 
 export default function Home() {
   const auth = useUser();//make a call to BE for useUser
   if (auth.user.email) return <Redirect to="/main" />;
 
   return (
-    <section>
-      <h1>Welcome to Life Stats.... </h1>
-      <p>Description of app goes here..</p> 
-      <p>
+    <>
+    <section className={Styles.titleSection}>
+      <h1 className={Styles.title}>Life Stats </h1>
+      </section>
+      <section className={Styles.imgSection}>
+        <img className={Styles.image}src='https://freesvg.org/img/1539121879.png' />
+      </section>
+    <div className={Styles.div}>
+      <section className={Styles.section}>
+      <p className={Styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p> 
+      <h2>
         Please <Link to="/login">sign in</Link>.
-      </p>
-    </section>
+      </h2>
+      </section>
+    </div>
+    </>
   );
 }
