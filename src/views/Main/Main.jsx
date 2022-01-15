d6d37390b0eb9014c74fbf7a90ee820d22d8a6be
simonkaine <simonkaine@gmail.com>
2022-01-15 14:49:00 -0800

    Push test.

--- src/views/Main/Main.jsx
+++ src/views/Main/Main.jsx
@@ -6,9 +6,9 @@ export default function Main() {
   // const [selectedBirthDate, setSelectedBirthDate] = useState(null);
   const [date, setDate] = useState('');
   const [breathsState, setBreathsState] = useState('');
-  const [day, setDay] = useState('');
-  const [month, setMonth] = useState('');
-  const [year, setYear] = useState('');
+  // const [day, setDay] = useState('');
+  // const [month, setMonth] = useState('');
+  // const [year, setYear] = useState('');
 
   let today = new Date();
   const dailySeconds = 86400;
@@ -16,45 +16,45 @@ export default function Main() {
   const yearlySeconds = 31536000;
   const breathsPerSecond = 0.25;
 
-  class Date {
-      constructor(d,m,y) {
-          this.d = d;
-          this.m = m;
-          this.y = y;
-      }
-  }
+  // class Date {
+  //     constructor(d,m,y) {
+  //         this.d = d;
+  //         this.m = m;
+  //         this.y = y;
+  //     }
+  // }
  
-  let monthDays=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+  // let monthDays=[31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
                         
-  function countLeapYears(d) {
-      let years = d.y;
+  // function countLeapYears(d) {
+  //     let years = d.y;
     
-        if (d.m <= 2) {
-          years--;
-        }
-        return Math.floor(years / 4) - Math.floor(years / 100) +
-          Math.floor(years / 400);
-  }
+  //       if (d.m <= 2) {
+  //         years--;
+  //       }
+  //       return Math.floor(years / 4) - Math.floor(years / 100) +
+  //         Math.floor(years / 400);
+  // }
  
-  function getDifference(dt1,dt2) {
-      let n1 = dt1.y * 365 + dt1.d;
+  // function getDifference(dt1,dt2) {
+  //     let n1 = dt1.y * 365 + dt1.d;
   
-      for (let i = 0; i < dt1.m - 1; i++) {
-        n1 += monthDays[i];
-      }
-      n1 += countLeapYears(dt1);
+  //     for (let i = 0; i < dt1.m - 1; i++) {
+  //       n1 += monthDays[i];
+  //     }
+  //     n1 += countLeapYears(dt1);
     
-      let n2 = dt2.y * 365 + dt2.d;
-      for (let i = 0; i < dt2.m - 1; i++) {
-        n2 += monthDays[i];
-      }
-      n2 += countLeapYears(dt2);
-      return (n2 - n1);
-  }
+  //     let n2 = dt2.y * 365 + dt2.d;
+  //     for (let i = 0; i < dt2.m - 1; i++) {
+  //       n2 += monthDays[i];
+  //     }
+  //     n2 += countLeapYears(dt2);
+  //     return (n2 - n1);
+  // }
 
-  let dt1 = new Date(breathsState);
-  let dt2 = new Date(today);
-  console.log(getDifference(dt1, dt2));
+  // let dt1 = new Date(breathsState);
+  // let dt2 = new Date(today);
+  // console.log(getDifference(dt1, dt2));
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -62,7 +62,7 @@ export default function Main() {
       let newDate = date;
       let splitDate = newDate.split('-'); // ex: ['2022', '11', '03']
       let dateFormat = splitDate.reverse(); // ex: ['01', '10', '1988']
-      return dateFormat.toString();
+      return dateFormat;
     }
     setBreathsState(mungedDate(date));
   };
