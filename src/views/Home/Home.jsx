1eb1aae51b76803a7b53eaa8d7b7339785ab1e43
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2022-01-18 16:57:11 -0800

    demo

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,61 +1,34 @@
 import { Link, Redirect } from 'react-router-dom';
-import React from 'react';
 import { useUser } from '../../context/UserContext';
-import Styles from './Home.css';
+import Styles from './Home.css'
 
 export default function Home() {
-  const auth = useUser(); //make a call to BE for useUser
-  // const [user, setUser] = useState('');
-  if (auth.user.email){
-    setUser(auth)
-    return <Redirect to="/main" />;
-  } 
+  const auth = useUser();//make a call to BE for useUser
+  if (auth.user.email) return <Redirect to="/main" />;
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (auth) {
-      window.location.href = 'https://pedantic-pasteur-453987.netlify.app/main';
+    if(auth) {
+      window.location.href = 'https://pedantic-pasteur-453987.netlify.app/main'
+      
     } else {
-      window.location.href =
-        'https://pedantic-pasteur-453987.netlify.app/login';
+      window.location.href = 'https://pedantic-pasteur-453987.netlify.app/login';
     }
-  };
+  }
 
   return (
     <>
-      <div className={Styles.titleDiv}>
-        <h1 className={Styles.title}>Life Stats</h1>
+    <div className={Styles.titleDiv}>
+       <h1 className={Styles.title}>Life Stats</h1>
+       </div>
+    <div className={Styles.div}>
+      <section className={Styles.section}>
+      <p className={Styles.desc}>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p> 
+      <div className={Styles.btnDiv}>
+      <button onClick={handleSubmit} className={Styles.signin}>PLAY</button>
       </div>
-      <div className={Styles.div}>
-        <section className={Styles.section}>
-          <p className={Styles.desc}>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem
-            ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-            tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua.
-          </p>
-
-          <div className={Styles.btnDiv}>
-            {auth ? (
-              <button onClick={handleSubmit} className={Styles.signin}>
-                PLAY
-              </button>
-            ) : (
-              <button onClick={handleSubmit} className={Styles.signin}>
-                SIGN IN
-              </button>
-            )}
-          </div>
-
-          <div className={Styles.btnDiv}>
-            <button onClick={handleSubmit} className={Styles.signin}>
-              SIGN IN
-            </button>
-          </div>
-        </section>
-      </div>
+      </section>
+    </div>
     </>
   );
 }
