c2f05fdf431772512d9bf91949a7308c8cc76d4b
simonkaine <simonkaine@gmail.com>
2022-01-21 11:09:49 -0800

    About us extra.

--- src/views/About/AboutUs.jsx
+++ src/views/About/AboutUs.jsx
@@ -15,42 +15,37 @@ export default function AboutUs() {
 
       <section className={Styles.section}>
         <div className={Styles.left}>
+          <p className={Styles.name}>Simon Kaine</p>
           <img src={Simon} />
           <p className={Styles.text}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum,
-            repellendus praesentium. Quam nobis, non aperiam sint aut possimus
-            dolorem, beatae, cum vel sunt saepe ad nostrum eius expedita eum
-            nulla!
+          Hi! I am a Full Stack Software Engineer currently living in Milwaukie, Oregon. Born on May 31st 1988 when the number one song in the US that week was 'One More Try' by George Michael. 
           </p>
         </div>
 
         <div className={Styles.right}>
+        <p className={Styles.name}>Aemilius Morgan</p>
+          <img src={Millie} />
           <p className={Styles.text}>
             Hello! I am a software engineer located in Portland OR. an
             interesting fact about me is that I am left handed, but I can use
             both fairly well.
           </p>
-          <img src={Millie} />
         </div>
 
         <div className={Styles.left}>
+        <p className={Styles.name}>Peter Montano</p>
           <img src={Peter} />
           <p className={Styles.text}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum,
-            repellendus praesentium. Quam nobis, non aperiam sint aut possimus
-            dolorem, beatae, cum vel sunt saepe ad nostrum eius expedita eum
-            nulla!
+          Full Stack Software Developer currently living in Portland, Oregon. Born on August 12th 1998, the year of the tiger.
           </p>
         </div>
 
         <div className={Styles.right}>
-          <p className={Styles.text}>
-            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum,
-            repellendus praesentium. Quam nobis, non aperiam sint aut possimus
-            dolorem, beatae, cum vel sunt saepe ad nostrum eius expedita eum
-            nulla!
-          </p>
+        <p className={Styles.name}>Dylan Greathouse</p>
           <img src={Dylan} />
+          <p className={Styles.text}>
+          Developing a passion for front and back end web development. Love any opportunity to design and create. Always looking for a chance to learn and be a better version of myself than I was the day before. Strong interest in game design and cooking.
+          </p>
         </div>
       </section>
     </div>
